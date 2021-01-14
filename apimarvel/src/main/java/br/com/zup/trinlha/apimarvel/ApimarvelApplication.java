d28df80323de7199ff9bec9c81eb046d4d95fe67
GrazielaRO <grazi375@hotmail.com>
2021-01-14 09:43:22 -0300

    Project created

---
+++ apimarvel/src/main/java/br/com/zup/trinlha/apimarvel/ApimarvelApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.trinlha.apimarvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimarvelApplication.class, args);
+	}
+
+}
---
+++ apimarvel/src/test/java/br/com/zup/trinlha/apimarvel/ApimarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.trinlha.apimarvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
